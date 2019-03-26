2cf32032392122fa38137205aa191e9e83009aae
Martin Outzen Berild <martinob@null05.math.ntnu.no>
2019-03-26 10:06:28 +0100

    Add figures folder with boxplot and a) answer

--- code/problem2.R
+++ code/problem2.R
@@ -1,2 +1,22 @@
-#setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-#rm(list = ls())
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+rm(list = ls())
+
+## --- labsdata2
+library(tidyverse)
+library(plotly)
+bili.df <- as.data.frame(read.table("../data/bilirubin.txt",header=T))
+
+## --- boxplot.bili
+boxplot.bili <- ggplot(bili.df, aes(x= pers, y = log(meas),fill=pers)) + 
+  geom_boxplot()+
+  ggtitle("boxplot of bilirubin data")
+
+## --- break
+ggsave("../figures/boxplot_bili.pdf", plot = boxplot.bili, device = NULL, 
+       path = NULL,scale = 1, width = 5.5, height = 2*4, 
+       units = "in",dpi = 300, limitsize = TRUE)
+
+## --- linearReg2
+log.y <- lm(log(meas)~pers,data=bili.df)
+sum.log.y <- summary(log.y)
+Fval <- sum.log.y$fstatistic
